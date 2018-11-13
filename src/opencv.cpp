d06f2c1707f18b360467d6a5afe139acfa93c0c4
Jun Chen <junc76@gmail.com>
2018-11-13 17:33:19 +0800

    Update opencv.cpp

--- src/opencv.cpp
+++ src/opencv.cpp
@@ -2,12 +2,10 @@
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
 TEST(opencv, load_image)
 {
     // https://www.opencv-srf.com/2017/11/load-and-display-image.html
-    Mat image = imread("./data/Einstein.jpg");
+    cv::Mat image = cv::imread("./data/Einstein.jpg");
 
     // Check for failure
     if (image.empty())
@@ -17,11 +15,10 @@ TEST(opencv, load_image)
         return;
     }
 
-    String windowName = "Einstein"; // Name of the window
-    namedWindow(windowName);        // Create a window
+    cv::String windowName = "Einstein"; // Name of the window
     imshow(windowName, image);      // Show our image inside the created window.
-    waitKey(0);                     // Wait for any keystroke in the window
-    destroyWindow(windowName);      // destroy the created window
+    cv::waitKey(0);                     // Wait for any keystroke in the window
+    cv::destroyWindow(windowName);      // destroy the created window
 }
 
 TEST(opencv, create_blank_image)
@@ -33,19 +30,18 @@ TEST(opencv, create_blank_image)
     // 800 x 600 of resolution (800 wide and 600 high)
     // each pixels initialized to the value of (100, 250, 30) for Blue, Green and Red planes
     // respectively.
-    Mat image(600, 800, CV_8UC3, Scalar(100, 250, 30));
+    cv::Mat image(600, 800, CV_8UC3, cv::Scalar(100, 250, 30));
 
-    String windowName = "Window with Blank Image"; // Name of the window
-    namedWindow(windowName);                       // Create a window
+    cv::String windowName = "Window with Blank Image"; // Name of the window
     imshow(windowName, image);                     // Show our image inside the created window.
-    waitKey(0);                                    // Wait for any keystroke in the window
-    destroyWindow(windowName);                     // destroy the created window
+    cv::waitKey(0);                                    // Wait for any keystroke in the window
+    cv::destroyWindow(windowName);                     // destroy the created window
 }
 
 TEST(opencv, gaussian_blur)
 {
     // Read the image file
-    Mat image = imread("./data/Einstein.jpg");
+    cv::Mat image = cv::imread("./data/Einstein.jpg");
 
     // Check for failure
     if (image.empty())
@@ -56,36 +52,31 @@ TEST(opencv, gaussian_blur)
     }
 
     // Blur the image with 3x3 Gaussian kernel
-    Mat image_blurred_with_3x3_kernel;
-    GaussianBlur(image, image_blurred_with_3x3_kernel, Size(3, 3), 0);
+    cv::Mat image_blurred_with_3x3_kernel;
+    GaussianBlur(image, image_blurred_with_3x3_kernel, cv::Size(3, 3), 0);
 
     // Blur the image with 5x5 Gaussian kernel
-    Mat image_blurred_with_5x5_kernel;
-    GaussianBlur(image, image_blurred_with_5x5_kernel, Size(5, 5), 0);
+    cv::Mat image_blurred_with_5x5_kernel;
+    cv::GaussianBlur(image, image_blurred_with_5x5_kernel, cv::Size(5, 5), 0);
 
     // Define names of the windows
-    String window_name = "Einstein";
-    String window_name_blurred_with_3x3_kernel = "Lotus Blurred with 3 x 3 Gaussian Kernel";
-    String window_name_blurred_with_5x5_kernel = "Lotus Blurred with 5 x 5 Gaussian Kernel";
-
-    // Create windows with above names
-    namedWindow(window_name);
-    namedWindow(window_name_blurred_with_3x3_kernel);
-    namedWindow(window_name_blurred_with_5x5_kernel);
+    cv::String window_name = "Einstein";
+    cv::String window_name_blurred_with_3x3_kernel = "Lotus Blurred with 3 x 3 Gaussian Kernel";
+    cv::String window_name_blurred_with_5x5_kernel = "Lotus Blurred with 5 x 5 Gaussian Kernel";
 
     // Show our images inside the created windows.
     imshow(window_name, image);
     imshow(window_name_blurred_with_3x3_kernel, image_blurred_with_3x3_kernel);
     imshow(window_name_blurred_with_5x5_kernel, image_blurred_with_5x5_kernel);
-    waitKey(0);          // Wait for any keystroke in the window
-    destroyAllWindows(); // destroy all opened windows
+    cv::waitKey(0);          // Wait for any keystroke in the window
+    cv::destroyAllWindows(); // destroy all opened windows
 }
 
 TEST(opencv, gray_image)
 {
     // http://www.bogotobogo.com/OpenCV/opencv_3_tutorial_load_convert_save_image.php
     // Read the image file
-    Mat image = imread("./data/Dostoevsky.jpg");
+    cv::Mat image = cv::imread("./data/Dostoevsky.jpg");
 
     // Check for failure
     if (image.empty())
@@ -96,18 +87,14 @@ TEST(opencv, gray_image)
     }
 
     // create a MAT object for gray image
-    Mat gray_image;
+    cv::Mat gray_image;
 
     // convert to Greyscale format
     // cvtColor( image, gray_image, CV_BGR2GRAY );
-    cvtColor( image, gray_image, COLOR_BGR2GRAY );
+    cvtColor( image, gray_image, cv::COLOR_BGR2GRAY );
 
     // save the transformed image to a file
     imwrite( "../images/GrayImage.jpg", gray_image );
-
-    // creates two windows
-    namedWindow( "Dostoevsky", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
 
     // imshow() displays an image in the specified window. 
     // If the window was created with the CV_WINDOW_AUTOSIZE flag, 
@@ -116,5 +103,28 @@ TEST(opencv, gray_image)
     imshow( "Gray image", gray_image );
 
     // wait for key press
-    waitKey(0);
+    cv::waitKey(0);
+}
+
+TEST(opencv, forEach_1_channel)
+{
+    typedef cv::Point3_<uint8_t> Pixel;
+
+    cv::Mat image(3, 2, CV_8UC1, cv::Scalar(0));
+    image.forEach<Pixel>([](Pixel &pixel, const int * position){
+        printf("[%d, %d] = %d\n", position[0], position[1], (int)pixel.x);
+    });
+}
+
+TEST(opencv, forEach_4_channel)
+{
+    using Pixel = cv::Point3_<uint8_t>;
+
+    cv::Mat image(3, 2, CV_8UC4, cv::Scalar(0,0,0,1));
+    image.forEach<Pixel>(
+        [&image](Pixel &pixel, const int * position){
+            const auto& v = image.at<cv::Vec4b>(position[0], position[1]);
+
+            printf("[%d, %d] = %d %d %d %d\n", position[0], position[1], v[0], v[1], v[2], v[3]);
+    });
 }
