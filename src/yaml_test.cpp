7a681c055ac7e8bf3d0fddb08e3ff486eff25591
Jun Chen <junc76@gmail.com>
2018-11-27 10:41:24 +0800

    yaml_test: use decode method to replace the operator >>

--- src/yaml_test.cpp
+++ src/yaml_test.cpp
@@ -27,31 +27,59 @@ struct Monster
     std::vector<Power> powers;
 };
 
-void operator>>(const YAML::Node &node, Vec3 &v)
+namespace YAML
 {
-    v.x = node[0].as<float>();
-    v.y = node[1].as<float>();
-    v.z = node[2].as<float>();
-}
-
-void operator>>(const YAML::Node &node, Power &power)
+template <> struct convert<Vec3>
 {
-    power.name = node["name"].as<std::string>();
-    power.damage = node["damage"].as<int>();
-}
-
-void operator>>(const YAML::Node &node, Monster &monster)
-{
-    node["position"] >> monster.position;
-    monster.name = node["name"].as<std::string>();
-    const YAML::Node &powers = node["powers"];
-    for (unsigned i = 0; i < powers.size(); i++)
+    static Node encode(const Vec3 &rhs)
     {
-        Power power;
-        powers[i] >> power;
-        monster.powers.push_back(power);
+        Node node;
+        node.push_back(rhs.x);
+        node.push_back(rhs.y);
+        node.push_back(rhs.z);
+        return node;
     }
-}
+
+    static bool decode(const Node &node, Vec3 &rhs)
+    {
+        if (!node.IsSequence() || node.size() != 3)
+        {
+            return false;
+        }
+
+        rhs.x = node[0].as<double>();
+        rhs.y = node[1].as<double>();
+        rhs.z = node[2].as<double>();
+        return true;
+    }
+};
+
+template <> struct convert<Power>
+{
+    static bool decode(const Node &node, Power &rhs)
+    {
+        rhs.name = node["name"].as<std::string>();
+        rhs.damage = node["damage"].as<int>();
+        return true;
+    }
+};
+
+template <> struct convert<Monster>
+{
+    static bool decode(const Node &node, Monster &rhs)
+    {
+        rhs.position = node["position"].as<Vec3>();
+        rhs.name = node["name"].as<std::string>();
+        const YAML::Node &powers = node["powers"];
+        for (unsigned i = 0; i < powers.size(); i++)
+        {
+            rhs.powers.emplace_back(powers[i].as<Power>());
+        }
+        return true;
+    }
+};
+
+} // namespace YAML
 
 // https://github.com/jbeder/yaml-cpp/wiki/Tutorial
 TEST(yaml, read_config_file)
@@ -61,8 +89,7 @@ TEST(yaml, read_config_file)
     {
         std::cout << doc[i] << "\n";
 
-        Monster monster;
-        doc[i] >> monster;
+        Monster monster = doc[i].as<Monster>();
         std::cout << rang::style::bold << rang::fg::gray << rang::bg::red << monster.name
                   << rang::style::reset << '\n';
     }
