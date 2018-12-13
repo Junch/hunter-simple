c1d38b7c7451b4fa0698090c1685875958ab52db
Jun Chen <junc76@gmail.com>
2018-12-13 10:48:06 +0800

    cpprest cannot be built well on windows. Exclude it from building on windows temporarily

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-cmake -H. -B_builds -DHUNTER_STATUS_DEBUG=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build _builds --config Debug
+cmake -H. -B_builds -DHUNTER_STATUS_DEBUG=ON -DCMAKE_BUILD_TYPE=Debug
+cmake --build _builds --config Debug --parallel 4 -- /verbosity:detailed
