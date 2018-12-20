fbf6d52c3d7b98e03d3bf300b2d8f1d1f547d525
Jun Chen <junc76@gmail.com>
2018-12-20 18:34:33 +0800

    Add more methods for test rest.stock

--- src/rxcpp/rest_stock.cpp
+++ src/rxcpp/rest_stock.cpp
@@ -7,6 +7,8 @@ using namespace web;
 using namespace web::http;
 using namespace web::http::experimental::listener;
 
+#define TRACE_ACTION(a, k, v) std::cout << a << " (" << k << ", " << v << ")\n"
+
 std::map<utility::string_t, utility::string_t> dictionary = 
 {
     {"SH510300", "300ETF"},
@@ -17,7 +19,7 @@ void display_json( json::value const & jvalue, utility::string_t const & prefix)
 {
    std::cout << prefix << jvalue.serialize() << '\n';
 }
- 
+
 void handle_get(http_request request)
 {
    LOG(INFO) << "handle GET";
@@ -34,6 +36,137 @@ void handle_get(http_request request)
    request.reply(status_codes::OK, answer);
 }
 
+void handle_request(
+   http_request request,
+   std::function<void(json::value const &, json::value &)> action)
+{
+   auto answer = json::value::object();
+ 
+   request
+      .extract_json()
+      .then([&answer, &action](pplx::task<json::value> task) {
+         try
+         {
+            auto const & jvalue = task.get();
+            display_json(jvalue, "R: ");
+ 
+            if (!jvalue.is_null())
+            {
+               action(jvalue, answer);
+            }
+         }
+         catch (http_exception const & e)
+         {
+            LOG(ERROR) << e.what();
+         }
+      })
+      .wait();
+    
+   display_json(answer, "S: ");
+ 
+   request.reply(status_codes::OK, answer);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  POST json data should be like ["SH510300", "SH601318", ...]
+///////////////////////////////////////////////////////////////////////////////
+void handle_post(http_request request)
+{
+   LOG(INFO) << "handle POST";
+ 
+   handle_request(
+      request,
+      [](json::value const & jvalue, json::value & answer)
+   {
+      for (auto const & e : jvalue.as_array())
+      {
+         if (e.is_string())
+         {
+            auto key = e.as_string();
+            auto pos = dictionary.find(key);
+ 
+            if (pos == dictionary.end())
+            {
+               answer[key] = json::value::string("<nil>");
+            }
+            else
+            {
+               answer[pos->first] = json::value::string(pos->second);
+            }
+         }
+      }
+   });
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  PUT json data should be like {"SZ000651": "格力电器"}
+///////////////////////////////////////////////////////////////////////////////
+void handle_put(http_request request)
+{
+   LOG(INFO) << "handle PUT";
+ 
+   handle_request(
+      request,
+      [](json::value const & jvalue, json::value & answer)
+   {
+      for (auto const & e : jvalue.as_object())
+      {
+         if (e.second.is_string())
+         {
+            auto key = e.first;
+            auto value = e.second.as_string();
+ 
+            if (dictionary.find(key) == dictionary.end())
+            {
+               TRACE_ACTION("added", key, value);
+               answer[key] = json::value::string("<put>");
+            }
+            else
+            {
+               TRACE_ACTION("updated", key, value);
+               answer[key] = json::value::string("<updated>");
+            }
+ 
+            dictionary[key] = value;
+         }
+      }
+   });
+}
+
+void handle_del(http_request request)
+{
+   LOG(INFO) << "handle DEL";
+ 
+   handle_request(
+      request,
+      [](json::value const & jvalue, json::value & answer)
+   {
+      std::set<utility::string_t> keys;
+      for (auto const & e : jvalue.as_array())
+      {
+         if (e.is_string())
+         {
+            auto key = e.as_string();
+ 
+            auto pos = dictionary.find(key);
+            if (pos == dictionary.end())
+            {
+               answer[key] = json::value::string("<failed>");
+            }
+            else
+            {
+               TRACE_ACTION("deleted", pos->first, pos->second);
+               answer[key] = json::value::string("<deleted>");
+               keys.insert(key);
+            }
+         }
+      }
+ 
+      for (auto const & key : keys)
+         dictionary.erase(key);
+   });
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // Title: Revisited: Full-fledged client-server example with C++ REST SDK 2.10
 // https://mariusbancila.ro/blog/2017/11/19/revisited-full-fledged-client-server-example-with-c-rest-sdk-2-10/
@@ -44,6 +177,9 @@ TEST(rest, stock)
     http_listener listener("http://127.0.0.1:8080/stockData");
 
     listener.support(methods::GET, handle_get);
+    listener.support(methods::POST, handle_post);
+    listener.support(methods::PUT,  handle_put);
+    listener.support(methods::DEL,  handle_del);
 
     try
     {
@@ -56,6 +192,6 @@ TEST(rest, stock)
     }
     catch (const std::exception &e)
     {
-        std::cout << e.what() << '\n';
+        LOG(ERROR) << e.what();
     }
 }
